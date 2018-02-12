3d11bd51a6aa9f196f2fdf661c2c6a151d9fd929
odorie <olivier.dorie@ign.fr>
2018-02-12 13:30:22 +0100

    ajout deps dans export_json rafale

--- scripts/export_json_rafale.sh
+++ scripts/export_json_rafale.sh
@@ -22,8 +22,10 @@ if [ ! -d ${data_path} ] ; then
    exit 1
 fi
 
+BinPath=`dirname $0`
+
 # on boucle par departement
-deps="01 02 03 04 05 07 08 09 10 11 12 13 15 16 17 18 19 2A 2B 21 23 24 25 26 28 30 31 32 34 36 37 39 40 41 42 43 44 45 46 47 48 49 51 52 53 54 55 57 58 59 60 62 63 64 65 66 69 70 71 72 73 74 75 77 79 80 81 82 83 84 86 87 88 89 92 93 94 95 971 972 973 974 975 976"
+deps="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 2A 2B 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 971 972 973 974 975 976 98"
 for dep in ${deps}
 do
 
@@ -32,7 +34,7 @@ do
    fi
 
    echo "Traitement du département ${dep}"
-   /home/ban/donnees-initiales-wip/scripts/export_json.sh ${data_path} ${dep} > cr${dep}.txt
+   ${BinPath}/export_json.sh ${data_path} ${dep} > cr${dep}.txt
 
    if [ $? -ne 0 ]
    then
